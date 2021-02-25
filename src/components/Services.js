c6aa3c6b16deed7ca8f53c6546d75bd308e0da30
Ulises Gomez <ulisesgomez120@gmail.com>
2021-02-24 18:05:55 -0600

    added packages and in person meeting

--- src/components/Services.js
+++ src/components/Services.js
@@ -4,8 +4,14 @@ import FbBanner from "./FbBanner";
 import ChatIcon from "../assets/icons/message-square.svg";
 import PhoneIcon from "../assets/icons/phone-call.svg";
 import VideoIcon from "../assets/icons/video.svg";
-
+import MeetingIcon from "../assets/icons/meeting.svg";
 const Container = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+const PackageContainer = styled.div`
   display: flex;
   width: 100%;
   justify-content: center;
@@ -17,6 +23,10 @@ const Card = styled.article`
   padding: 33px 24px;
   text-align: center;
   margin: 0 25px 25px 0;
+  max-width: 220px;
+  &.package-card {
+    max-width: 315px;
+  }
   @media (max-width: 460px) {
     margin: 25px 0;
   }
@@ -33,17 +43,24 @@ const Icon = styled.img`
   padding: 15px;
   border-radius: 50%;
 `;
-const Amount = styled.p`
+const Price = styled.p`
   font-size: 21px;
   font-weight: bold;
   margin-bottom: 11px;
 `;
+
 const Heading = styled.h2`
   font-family: "Cormorant Garamond", serif;
   text-align: center;
   font-size: 29px;
   margin-bottom: 30px;
   text-transform: capitalize;
+  &.package-heading {
+    margin-top: 22px;
+  }
+`;
+const P = styled.p`
+  margin-bottom: 3px;
 `;
 const Section = styled.section`
   padding: 30px 15px;
@@ -51,31 +68,56 @@ const Section = styled.section`
     padding: 30px 55px;
   }
 `;
-const Services = ({ heading, data, hosted, language }) => {
+const Services = ({ heading, services, packages, language }) => {
   const iconUrls = {
     Chat: ChatIcon,
     "Chat De Texto": ChatIcon,
     Phone: PhoneIcon,
     Teléfono: PhoneIcon,
     Video: VideoIcon,
+    "In Person": MeetingIcon,
+    "En Persona": MeetingIcon,
   };
-  const servicesJsx = data.map(({ name, description, amount, duration }, i) => {
-    return (
-      <Card key={`${i}${name}`}>
-        <Name>{name}</Name>
-        <Icon src={iconUrls[name]} alt="name" />
-        <Amount>{amount}</Amount>
-        <p>
-          {duration} {description}
-        </p>
-        <p>{hosted}</p>
-      </Card>
-    );
-  });
+  const servicesJsx = services.map(
+    ({ name, description, price, duration, hosted }, i) => {
+      return (
+        <Card key={`${i}${name}`}>
+          <Name>{name}</Name>
+          <Icon src={iconUrls[name]} alt='name' />
+          <Price>{price}</Price>
+          <P>
+            {duration} {description}
+          </P>
+          <P>{hosted}</P>
+        </Card>
+      );
+    }
+  );
+  const bundlesJsx = packages.map(
+    ({ name, price, description, hosted, savings }, i) => {
+      return (
+        <Card key={`${i}${name}`} className='package-card'>
+          <Name>{name}</Name>
+          {/* <Icon src={iconUrls[name]} alt='name' /> */}
+          <Price>{price}</Price>
+          <P>{description}</P>
+          <h3 style={{ marginBottom: "3px", fontSize: "16px" }}>
+            {language === "en" ? "Savings: " : "Ahorra "}
+            {savings}
+          </h3>
+          <P>{hosted}</P>
+        </Card>
+      );
+    }
+  );
   return (
     <Section>
       <Heading>{heading}</Heading>
       <Container>{servicesJsx}</Container>
+      <Heading className='package-heading'>
+        {language === "en" ? "New Bundles!" : "Paquetes Nuevos!"}
+      </Heading>
+      <PackageContainer>{bundlesJsx}</PackageContainer>
       <FbBanner language={language} />
     </Section>
   );
